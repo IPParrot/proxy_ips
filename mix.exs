4f60e055931d0e91a79d408f3be937b2049ce005
Dairon Medina Caro <dairon.medina@gmail.com>
2025-12-29 17:55:42 -0500

    fix: escript

--- mix.exs
+++ mix.exs
@@ -5,10 +5,11 @@ defmodule ProxyIps.MixProject do
     [
       app: :proxy_ips,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: escript()
+      escript: escript(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -25,6 +26,9 @@ defmodule ProxyIps.MixProject do
       {:flow, "~> 1.2"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp escript do
     [main_module: ProxyIps.CLI]
