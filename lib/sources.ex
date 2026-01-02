44f0ee56bf9a368f75c45f5002d6d87bff9c3edc
Dairon Medina Caro <dairon.medina@gmail.com>
2026-01-02 11:12:01 -0500

    fix: update proxy lists

--- lib/sources.ex
+++ lib/sources.ex
@@ -15,7 +15,7 @@ defmodule ProxyIps.Sources do
       "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/socks5_proxies.txt",
       "https://raw.githubusercontent.com/Argh94/Proxy-List/refs/heads/main/SOCKS5.txt",
       "https://raw.githubusercontent.com/Firmfox/Proxify/refs/heads/main/proxies/socks5.txt",
-      "https://github.com/zloi-user/hideip.me/raw/refs/heads/master/socks5.txt",
+      "https://raw.githubusercontent.com/zloi-user/hideip.me/refs/heads/master/socks5.txt",
       "https://raw.githubusercontent.com/ClearProxy/checked-proxy-list/main/socks5/raw/all.txt",
       "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/refs/heads/main/proxies/socks5.txt"
     ]
@@ -33,7 +33,7 @@ defmodule ProxyIps.Sources do
       "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/socks4_proxies.txt",
       "https://raw.githubusercontent.com/Argh94/Proxy-List/refs/heads/main/SOCKS4.txt",
       "https://raw.githubusercontent.com/Firmfox/Proxify/refs/heads/main/proxies/socks4.txt",
-      "https://github.com/zloi-user/hideip.me/raw/refs/heads/master/socks4.txt",
+      "https://raw.githubusercontent.com/zloi-user/hideip.me/refs/heads/master/socks4.txt",
       "https://raw.githubusercontent.com/ClearProxy/checked-proxy-list/main/socks4/raw/all.txt",
       "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/refs/heads/main/proxies/socks4.txt"
     ]
@@ -54,7 +54,7 @@ defmodule ProxyIps.Sources do
       "https://raw.githubusercontent.com/mthcht/awesome-lists/refs/heads/main/Lists/PROXY/ALL_PROXY_Lists.csv",
       "https://raw.githubusercontent.com/mmpx12/proxy-list/refs/heads/master/proxies.txt",
       "https://raw.githubusercontent.com/mmpx12/proxy-list/refs/heads/master/http.txt",
-      "https://github.com/zloi-user/hideip.me/raw/refs/heads/master/http.txt",
+      "https://raw.githubusercontent.com/zloi-user/hideip.me/refs/heads/master/http.txt",
       "https://raw.githubusercontent.com/ClearProxy/checked-proxy-list/main/http/raw/all.txt",
       "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/refs/heads/main/proxies/http.txt"
     ]
@@ -68,7 +68,7 @@ defmodule ProxyIps.Sources do
       "https://raw.githubusercontent.com/zebbern/Proxy-Scraper/refs/heads/main/https.txt",
       "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/https.txt",
       "https://raw.githubusercontent.com/Firmfox/Proxify/refs/heads/main/proxies/https.txt",
-      "https://github.com/zloi-user/hideip.me/raw/refs/heads/master/https.txt",
+      "https://raw.githubusercontent.com/zloi-user/hideip.me/refs/heads/master/https.txt",
       "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/refs/heads/main/proxies/https.txt"
     ]
   end
